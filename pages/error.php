03d044103a6822b57fc5e4e80c8d547207785164
radiantBear <85893453+radiantBear@users.noreply.github.com>
2024-06-12 15:37:56 -0700

    Improve error page with back button

--- pages/error.php
+++ pages/error.php
@@ -35,6 +35,7 @@ echo "
             <p style='white-space: initial'>$message</p>
             <p class='d-inline'><b>NOTE: </b>If you believe this site contains an error, please click the orange feeedback button to submit an issue report.</p>
             <a href='$baseUrl' class='btn btn-primary float-right'>Home Page</a>
+            <button type='button' class='btn btn-outline-primary mx-2 float-right' onclick='window.history.back();'>Back</button>
         </div>
     </div>
 </div>
